f1b0ee89b6efe394d983d66d6d334087615fee1d
Mohamed Esskely <messkely@e1r3p4.1337.ma>
2023-11-25 15:39:39 +0100

    ff

---
+++ my-libft/ft_strjoin.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: messkely <messkely@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 12:14:27 by messkely          #+#    #+#             */
+/*   Updated: 2023/11/21 14:07:36 by messkely         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	*ft_strcat(char *dst, const char *src)
+{
+	int	dl;
+	int	sl;
+	int	j;
+
+	dl = ft_strlen(dst);
+	sl = ft_strlen(src);
+	j = 0;
+	while (j < sl)
+	{
+		dst[dl] = src[j];
+		dl++;
+		j++;
+	}
+	dst[dl] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	len;
+	char	*ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	len = s1_len + s2_len;
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (!ptr)
+		return (0);
+	ft_strlcpy(ptr, s1, s1_len + 1);
+	ft_strcat(ptr, s2);
+	return (ptr);
+}
